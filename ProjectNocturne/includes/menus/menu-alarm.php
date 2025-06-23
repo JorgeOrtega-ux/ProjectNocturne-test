e32aaa5cb98dbaafdc0ec892231dc3a5e6fd2b0a
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-06-22 18:36:16 -0600

    Add files via upload

--- ProjectNocturne/includes/menus/menu-alarm.php
+++ ProjectNocturne/includes/menus/menu-alarm.php
@@ -9,7 +9,7 @@
                     <span class="material-symbols-rounded">search</span>
                 </div>
                 <div class="search-content-text">
-                    <input type="text" class="body-title" autocomplete="off"> <!--este input de busuqedano lo toques ni agregues funciones, nada, es para despues -->
+                    <input type="text" class="body-title" autocomplete="off" placeholder="Buscar alarmas...">
                 </div>
             </div>
         </div>
@@ -19,39 +19,39 @@
                     <div class="menu-content-header">
                         <div class="menu-content-header-primary">
                             <span class="material-symbols-rounded">tag</span>
-                            <span>Titulo de la alarma</span>
+                            <span>Título de la alarma</span>
                         </div>
                     </div>
                     <div class="menu-content-general">
                         <div class="enter-text-tool">
-                            <input type="text">
+                            <input type="text" id="alarm-title" placeholder="Mi nueva alarma">
                         </div>
                     </div>
                 </div>
                 <div class="menu-content">
                     <div class="menu-content-header">
                         <div class="menu-content-header-primary">
-                            <span class="material-symbols-rounded">tag</span>
-                            <span>Configuracion de la alarma</span>
+                            <span class="material-symbols-rounded">schedule</span>
+                            <span>Configuración de la alarma</span>
                         </div>
                     </div>
                     <div class="menu-content-general">
                         <div class="enter-date-content">
                             <div class="enter-date-tool">
-                                <div class="enter-date-tool-left">
+                                <div class="enter-date-tool-left" data-action="decreaseHour">
                                     <span class="material-symbols-rounded">arrow_left</span>
                                 </div>
-                                <div class="enter-date-tool-center">12 horas</div>
-                                <div class="enter-date-tool-right">
+                                <div class="enter-date-tool-center" id="hour-display">12 horas</div>
+                                <div class="enter-date-tool-right" data-action="increaseHour">
                                     <span class="material-symbols-rounded">arrow_right</span>
                                 </div>
                             </div>
                             <div class="enter-date-tool">
-                                <div class="enter-date-tool-left">
+                                <div class="enter-date-tool-left" data-action="decreaseMinute">
                                     <span class="material-symbols-rounded">arrow_left</span>
                                 </div>
-                                <div class="enter-date-tool-center">0 minutos</div>
-                                <div class="enter-date-tool-right">
+                                <div class="enter-date-tool-center" id="minute-display">0 minutos</div>
+                                <div class="enter-date-tool-right" data-action="increaseMinute">
                                     <span class="material-symbols-rounded">arrow_right</span>
                                 </div>
                             </div>
@@ -61,50 +61,77 @@
                 <div class="menu-content">
                     <div class="menu-content-header">
                         <div class="menu-content-header-primary">
-                            <span class="material-symbols-rounded">tag</span>
+                            <span class="material-symbols-rounded">music_note</span>
                             <span>Sonido de la alarma</span>
                         </div>
                     </div>
                     <div class="menu-content-general">
                         <div class="enter-sound-wrapper">
-                            <div class="enter-sound-content">
+                            <div class="enter-sound-content" data-action="toggleSoundMenu">
                                 <div class="enter-sound-content-left">
-                                    <span>Sonido predeterminado</span>
+                                    <span id="selected-sound">Beep Clásico</span>
                                 </div>
                                 <div class="enter-sound-content-right">
-                                    <span class="material-symbols-rounded">
-                                        expand_more
-                                    </span>
+                                    <span class="material-symbols-rounded">expand_more</span>
                                 </div>
                             </div>
-                            <div class="menu-sound disabled">
+                            <div class="menu-sound disabled" data-menu="sound-menu">
                                 <div class="menu-list">
-                                    <div class="menu-link">
-                                        <div class="menu-link-icon"></div>
-                                        <div class="menu-link-text"></div>
+                                    <div class="menu-link" data-action="selectSound" data-sound="classic-beep">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">volume_up</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Beep Clásico</span>
+                                        </div>
                                     </div>
-                                      <div class="menu-link">
-                                        <div class="menu-link-icon"></div>
-                                        <div class="menu-link-text"></div>
+                                    <div class="menu-link" data-action="selectSound" data-sound="gentle-chime">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">notifications</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Campanita Suave</span>
+                                        </div>
                                     </div>
-                                      <div class="menu-link">
-                                        <div class="menu-link-icon"></div>
-                                        <div class="menu-link-text"></div>
+                                    <div class="menu-link" data-action="selectSound" data-sound="digital-alarm">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">alarm</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Alarma Digital</span>
+                                        </div>
+                                    </div>
+                                    <div class="menu-link" data-action="selectSound" data-sound="peaceful-tone">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">self_care</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Tono Relajante</span>
+                                        </div>
+                                    </div>
+                                    <div class="menu-link" data-action="selectSound" data-sound="urgent-beep">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">priority_high</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Beep Urgente</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div class="menu-alarm-button" data-action="previewSound">
+                                <span class="material-symbols-rounded">play_arrow</span>
+                            </div>
                         </div>
-                        <div class="menu-alarm-button">
-                          <span class="material-symbols-rounded">
-play_arrow
-</span>
-                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
         <div class="menu-section-bottom">
-            <div class="create-tool" data-action="createAlarm"><span>Crear alarma</span></div>
+            <div class="create-tool" data-action="createAlarm">
+                <span>Crear alarma</span>
+            </div>
         </div>
     </div>
 </div>
--- ProjectNocturne/includes/menus/menu-worldClock.php
+++ ProjectNocturne/includes/menus/menu-worldClock.php
@@ -1,19 +1,120 @@
-    <div class="menu-worldClock disabled" data-menu="WorldClock">
-        <div class="pill-container">
-            <div class="drag-handle"></div>
+<div class="menu-worldClock disabled" data-menu="WorldClock">
+    <div class="pill-container">
+        <div class="drag-handle"></div>
+    </div>
+    <div class="menu-section">
+        <div class="menu-section-top">
+            <div class="search-content">
+                <div class="search-content-icon">
+                    <span class="material-symbols-rounded">search</span>
+                </div>
+                <div class="search-content-text">
+                    <input type="text" class="body-title" autocomplete="off">
+                </div>
+            </div>
         </div>
-        <div class="menu-section">
-            <div class="menu-section-top">
-                <div class="search-content">
-                    <div class="search-content-icon">
-                        <span class="material-symbols-rounded">search</span>
+        <div class="menu-section-center">
+            <div class="menu-content-wrapper active">
+                <div class="menu-content">
+                    <div class="menu-content-header">
+                        <div class="menu-content-header-primary">
+                            <span class="material-symbols-rounded">tag</span>
+                            <span>Titulo</span>
+                        </div>
                     </div>
-                    <div class="search-content-text">
-                        <input type="text" class="body-title" autocomplete="off">
+                    <div class="menu-content-general">
+                        <div class="enter-text-tool">
+                            <input type="text" id="clock-title" placeholder="Nuevo reloj">
+                        </div>
+                    </div>
+                </div>
+                <div class="menu-content">
+                    <div class="menu-content-header">
+                        <div class="menu-content-header-primary">
+                            <span class="material-symbols-rounded">tag</span>
+                            <span>Selecciona un pais</span>
+                        </div>
+                    </div>
+                    <div class="menu-content-general">
+                        <div class="enter-sound-wrapper">
+                            <div class="enter-sound-content" data-action="toggleSoundMenu">
+                                <div class="enter-sound-content-left">
+                                    <span id="selected-sound">Seleccionar un pais</span>
+                                </div>
+                                <div class="enter-sound-content-right">
+                                    <span class="material-symbols-rounded">expand_more</span>
+                                </div>
+                            </div>
+                            <div class="menu-sound disabled" data-menu="countryMenu">
+                                <div class="menu-list">
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded"></span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>pais 1</span>
+                                        </div>
+                                    </div>
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded"></span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>pais 2</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="menu-content">
+                    <div class="menu-content-header">
+                        <div class="menu-content-header-primary">
+                            <span class="material-symbols-rounded">tag</span>
+                            <span>Selecciona una zona horaria</span>
+                        </div>
+                    </div>
+                    <div class="menu-content-general">
+                        <div class="enter-sound-wrapper">
+                            <div class="enter-sound-content" data-action="toggleSoundMenu">
+                                <div class="enter-sound-content-left">
+                                    <span id="selected-sound">Seleccionar un zona horaria</span>
+                                </div>
+                                <div class="enter-sound-content-right">
+                                    <span class="material-symbols-rounded">expand_more</span>
+                                </div>
+                            </div>
+                            <div class="menu-sound disabled" data-menu="countryMenu">
+                                <div class="menu-list">
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded"></span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>zona 1</span>
+                                        </div>
+                                    </div>
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded"></span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>zona 2</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="menu-section-center"></div>
-            <div class="menu-section-bottom disabled"></div>
+        </div>
+
+    </div>
+    <div class="menu-section-bottom">
+        <div class="create-tool" data-action="addClock">
+            <span>Agregar reloj</span>
         </div>
     </div>
+</div>
